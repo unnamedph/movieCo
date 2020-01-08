bdb1e214010bd0b90d3a233ef76cee46cfcc97de
Master Ace <gapotanimpuno@gmail.com>
2020-01-08 14:29:27 +0800

    add comparison...

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ createAutoComplete({
 	root: document.querySelector('#left-autocomplete'),
 	onOptionSelect(movie) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		onMovieSelect(movie, document.querySelector('#left-summary'));
+		onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
 	}
 });
 createAutoComplete({
@@ -39,11 +39,13 @@ createAutoComplete({
 	root: document.querySelector('#right-autocomplete'),
 	onOptionSelect(movie) {
 		document.querySelector('.tutorial').classList.add('is-hidden');
-		onMovieSelect(movie, document.querySelector('#right-summary'));
+		onMovieSelect(movie, document.querySelector('#right-summary', 'right'));
 	}
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+const onMovieSelect = async (movie, summaryElement, side) => {
 	const response = await axios.get('http://www.omdbapi.com/', {
 		params: {
 			apikey: 'd9835cc5',
@@ -52,6 +54,12 @@ const onMovieSelect = async (movie, summaryElement) => {
 	});
 
 	summaryElement.innerHTML = movieTemplate(response.data);
+
+	if (side === 'left') {
+		leftMovie = response.data;
+	} else {
+		rightMovie = response.data;
+	}
 };
 
 const movieTemplate = (movieDetail) => {
